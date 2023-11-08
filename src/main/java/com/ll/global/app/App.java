5a66b2865d3bd1984c66e4e00759983c86a98237
dongha922 <kdh4575@naver.com>
2023-11-08 16:05:38 +0900

    t3_2 , pass

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -11,7 +11,9 @@ public class App {
     public void run(){
         System.out.println("== 명언 앱 ==");
 
+
         System.out.println("명언 : ");
         System.out.println("작가 : ");
+        System.out.println("1번 명언이 등록되었습니다.");
     }
 }
